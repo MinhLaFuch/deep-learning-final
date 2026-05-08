b2c9faf9305f2c1c27fd87790758b808ffa1116a
Fuch <ph18122005@gmail.com>
2026-05-08 19:21:03 +0700

    remove utilities file

--- record.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("./viet-cultural-vqa/splits/train_data.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-# Xem record đầu tiên
-sample = data[0] if isinstance(data, list) else next(iter(data.values()))[0]
-print(json.dumps(sample, ensure_ascii=False, indent=2))
